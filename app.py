630b2d12acb0722ff43db2fe3864fc13d3777114
Saif Rizvi <saif1229@gmail.com>
2017-01-15 21:54:26 -0500

    Simplified to use MySQL procedures, website routes changed

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # Author: Saif Rizvi
 # RESTful URL shortening service 
 
-from flask import Flask, request, redirect, abort
+from flask import Flask, request, redirect, abort, jsonify
 from flaskext.mysql import MySQL
 import validators
 
@@ -17,80 +17,64 @@ app.config['MYSQL_DATABASE_DB'] = 'urlstorage'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
-# Handles incoming POST requests
-@app.route('/', methods = ['POST'])
-def api_root():
+# Handles incoming POST requests to shorten new URL
+@app.route('/services/shortener', methods = ['POST'])
+def api_shorten_requests():
 	
-	# There's probably a better way to do this
+	# Request must have a JSON body
 	if not request.json:
 		abort(400)
 	
-	# grab requested url from body
+	# Grab requested url from body
 	longURL = request.json['longURL']
 	
-	# verify valid url formatting. Note: Must start with http(s)://
+	# Verify valid url formatting
 	if not validators.url(longURL):
-		return request.jsonify('{error : "Please use full URL"}')
+		return jsonify(error="Please use valid URL. Remember to include http(s)://",longURL=longURL)
 
 	# Connect to DB and get cursor
 	cursor = mysql.connect().cursor()
 	
 	# Check if url already in db
-	cursor.execute('SELECT * FROM urls WHERE longURL ="' + longURL + '"')
+	cursor.callproc('GetRecordFromLongURL', [longURL])
 	data = cursor.fetchone()
 
-	# if data is None:
-		# cursor.callproc('AddNewURL', longURL)
+	if not data:
+		cursor.callproc('AddNewURL', [longURL])
+		cursor.callproc('GetRecordFromLongURL', [longURL])
+		data = cursor.fetchone()
+	
+	cursor.close()
 
-	# return json object with details of new shortened URL
+	# Return json object with details of new shortened URL
+	return jsonify(shortURL=data[0], longURL=data[1])	
+
+# Make a separate path for API GET requests, returns info
+# @app.route('/api/v1/<shortURL>', )
 
 # Redirects user to URL corresponding to given shortURL if it exists
 @app.route('/<shortURL>', methods = ['GET'])
-def api_shortURL(shortURL):
+def api_get_shortURL(shortURL):
 
 	# Ignore weird request Chrome keeps sending
 	if "favicon" in shortURL:
 		pass
-	
-	# query urlDB for shortURL's corresponding longURL
+
+	# Query urlDB for shortURL's corresponding longURL
 	cursor = mysql.connect().cursor()
-	cursor.callproc('GetLongURL', shortURL)
+
+	cursor.callproc('GetRecordFromShortURL', [shortURL])
 	data = cursor.fetchone()
 	cursor.close()
 
 	if data is None:
 		return abort(404) # implement custom error response
 	else:
-		return redirect(data[0], 302) # is this unsafe?
+		return redirect(data[1], 302)
 
 # TODO: Check if url is in base 62 and 6 chars max
 def validateShortURL(url):
 	pass
-
-# base10 to base62 converter. Implement MySQL procedure to use instead of this.
-def base10ToBase62(urlID):
-	if urlID is 0:
-		return "0"
-
-	base62 = {0:0,1:1,2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:9,10:'a',
-				11:'b',12:'c',13:'d',14:'e',15:'f',16:'g',
-				17:'h',18:'i',19:'j',20:'k',21:'l',22:'m',
-				23:'n',24:'o',25:'p',26:'q',27:'r',28:'s',
-				29:'t',30:'u',31:'v',32:'w',33:'x',34:'y',
-				35:'z',36:'A',37:'B',38:'C',39:'D',40:'E',
-				41:'F',42:'G',43:'H',44:'I',45:'J',46:'K',
-				47:'L',48:'M',49:'N',50:'O',51:'P',52:'Q',
-				53:'R',54:'S',55:'T',56:'U',57:'V',58:'W',
-				59:'Z',60:'X', 61:'Y',62:'Z'}
-	
-	# Repeated division method
-	output = []
-	dividend = urlID
-	remainder = 0
-	while dividend > 0:
-		dividend, remainder = divmod(dividend,62)
-		output.append(base62[remainder]) 
-	return "".join[]
 
 if __name__ == '__main__':
 	app.run()
